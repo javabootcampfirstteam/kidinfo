cd4a6394a640a2a9fc4ae260081d78366bca8029
Mihail Klopotnuk <m.klopotnuk@gmail.com>
2019-02-11 00:32:19 +0300

    Bot can answer on '/start' command

--- src/main/java/KidinfoTelegraBot.java
+++ src/main/java/KidinfoTelegraBot.java
@@ -1,6 +1,9 @@
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 public class KidinfoTelegraBot extends TelegramLongPollingBot {
@@ -17,10 +20,27 @@ public class KidinfoTelegraBot extends TelegramLongPollingBot {
     }
 
     public void onUpdateReceived(Update update) {
-        
+        Message message = update.getMessage();
+        String textMessage = message.getText();
+        if (textMessage.equals("/start")){
+        sendMsg(message,"START");
+        }
+        else sendMsg(message,"What do you mean?");
     }
 
     public String getBotUsername() {
         return BOT_NAME;
+    }
+
+    private void sendMsg(Message outMessage, String txtMessage){
+        SendMessage s = new SendMessage();
+        s.setChatId(outMessage.getChatId());
+        s.setText(txtMessage);
+        try{
+            execute(s);
+        }
+        catch (TelegramApiException e){
+            e.printStackTrace();
+        }
     }
 }
