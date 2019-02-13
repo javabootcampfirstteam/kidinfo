7a922eb9854bd7a7ab08db993b1b42f2c2b04685
Mihail Klopotnuk <m.klopotnuk@gmail.com>
2019-02-13 20:21:03 +0300

    Add BotEvents model

--- src/main/java/model/BotEvent.java
+++ src/main/java/model/BotEvent.java
@@ -1,4 +1,25 @@
 package model;
 
-public class BotEvents {
+import java.time.LocalDateTime;
+
+public class BotEvent {
+
+    private String eventName;
+    private String eventType;
+    private LocalDateTime eventDateTime;
+    private String eventLocation;
+    private String enetContact;
+    private String eventPhone;
+    private String eventAdditional;
+
+
+    public BotEvent(String eventName, String eventType, LocalDateTime eventDateTime, String eventLocation, String enetContact, String eventPhone, String eventAdditional) {
+        this.eventName = eventName;
+        this.eventType = eventType;
+        this.eventDateTime = eventDateTime;
+        this.eventLocation = eventLocation;
+        this.enetContact = enetContact;
+        this.eventPhone = eventPhone;
+        this.eventAdditional = eventAdditional;
+    }
 }
